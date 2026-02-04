2cacdf046ccb8f7b85414af4b33db26cd8bf4cde
Joshua V. Dillon <jvdillon@gmail.com>
2026-02-04 11:34:11 -0800

    add type debug demo

--- tests/demo.py
+++ tests/demo.py
@@ -34,6 +34,9 @@ class SetupableMeta3(type):
         return cls
 
 
+# Note: Fig1 is the current choice of configgle.
+
+
 class Fig1(Setupable[_ParentT], metaclass=SetupableMeta1):
     pass
 
@@ -46,6 +49,32 @@ class Fig3(Setupable[_ParentT], metaclass=SetupableMeta3):
     pass
 
 
+# -------------------------------------------------------------------------------
+# Fully works! ...But requires effort from user.
+#
+# Type of "Foo.Config" is "type[Config]"
+# Type of "Foo.Config().x" is "int"
+# Type of "Foo.Config().setup()" is "Foo"
+
+
+class Foo:
+    class Config(Fig1["Foo"]):
+        x: int = 0
+
+
+reveal_type(Foo.Config)
+reveal_type(Foo.Config().x)
+reveal_type(Foo.Config().setup())
+
+
+# -------------------------------------------------------------------------------
+# No errors or warnings! ...But loses type from setup.
+#
+# Type of "Foo1.Config" is "type[Config]"
+# Type of "Foo1.Config().x" is "int"
+# Type of "Foo1.Config().setup()" is "Any"
+
+
 class Foo1:
     class Config(Fig1):
         x: int = 0
@@ -56,6 +85,14 @@ reveal_type(Foo1.Config().x)
 reveal_type(Foo1.Config().setup())
 
 
+# -------------------------------------------------------------------------------
+# Loses Config type.
+#
+# Type of "Foo2.Config" is "type[Setupable[Foo2]]"
+# Type of "Foo2.Config().x" is "Unknown"
+# Type of "Foo2.Config().setup()" is "Foo2"
+
+
 class Foo2:
     class Config(Fig2):
         x: int = 0
@@ -64,6 +101,14 @@ class Foo2:
 reveal_type(Foo2.Config)
 reveal_type(Foo2.Config().x)  # pyright: ignore[reportAttributeAccessIssue]
 reveal_type(Foo2.Config().setup())
+
+
+# -------------------------------------------------------------------------------
+# Loses Config type and setup type.
+#
+# Type of "Foo3.Config" is "type[Config] | type[Setupable[Foo3]]"
+# Type of "Foo3.Config().x" is "int | Unknown"
+# Type of "Foo3.Config().setup()" is "Foo3 | Any"
 
 
 class Foo3:
